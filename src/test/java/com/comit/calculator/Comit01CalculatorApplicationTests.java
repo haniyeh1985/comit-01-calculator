54d63f55df7593e3a97289fb375093f79809e8c7
Somayeh Abedi Isfahani <s.abedi.esfahani@gmail.com>
2023-04-21 14:15:40 -0600

    first commit

---
+++ src/test/java/com/comit/calculator/Comit01CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comit.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comit01CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
